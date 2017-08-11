0b776ae2a36f8d9548b4b70601a75f3b1cde9dd4
Justin Tomich <tomichj@gmail.com>
2017-08-11 15:18:15 -0700

    added spec for event subscritpion updated

--- spec/services/event_subscription_updated_spec.rb
+++ spec/services/event_subscription_updated_spec.rb
@@ -3,7 +3,12 @@ require 'spec_helper'
 module Influx
   describe EventSubscriptionUpdated do
     describe '#call' do
-
+      it 'synchronizes stripe to subscription' do
+        subscription = create(:subscription)
+        expect_any_instance_of(Influx::Subscription).to receive(:sync_with!)
+        event = StripeMock.mock_webhook_event('customer.subscription.updated', id: subscription.stripe_id)
+        Influx::EventSubscriptionUpdated.call(event)
+      end
     end
   end
 end
