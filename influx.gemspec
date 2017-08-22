b6d2548ef2e7a5a707b682dc9adc2d58c8763d5d
Justin Tomich <tomichj@gmail.com>
2017-08-22 16:32:32 -0700

    beefed up install generator, supports --subscriber now

--- influx.gemspec
+++ influx.gemspec
@@ -38,7 +38,4 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec-rails'
   spec.add_development_dependency 'factory_girl_rails'
   spec.add_development_dependency 'stripe-ruby-mock', '~> 2.3.1'
-
-  # spec.add_development_dependency 'rake'
-  # spec.add_development_dependency 'bundler'
 end
--- lib/generators/influx/install_generator.rb
+++ lib/generators/influx/install_generator.rb
@@ -1,9 +1,37 @@
 module Influx
   class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
+    class_option :subscriber,
+                 optional: true,
+                 type: :string,
+                 banner: 'subscriber',
+                 desc: "Specify the subscriber class name if you will use anything other than '::User'"
+
+    def initialize(*)
+      super
+      assign_names!(subscriber_class_name)
+    end
 
     def install_initializer
-      initializer 'influx.rb', File.read(File.expand_path('../templates/influx.rb', __FILE__))
+      copy_file 'influx.rb', 'config/initializers/influx.rb'
+      if options[:subscriber]
+        inject_into_file(
+          'config/initializers/influx.rb',
+          "  config.subscriber = '#{options[:subscriber]}'\n",
+          after: "Influx.configure do |config|\n"
+        )
+      end
+    end
+
+    private
+    def subscriber_class_name
+      options[:subscriber] ? options[:subscriber].classify : '::User'
+    end
+
+    def assign_names!(name)
+      @class_path = name.include?('/') ? name.split('/') : name.split('::')
+      @class_path.map!(&:underscore)
+      @file_name = @class_path.pop
     end
   end
 end
