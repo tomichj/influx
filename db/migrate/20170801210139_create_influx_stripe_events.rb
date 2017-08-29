f563a3fb53ceb30c34da691562452daffc966049
Justin Tomich <tomichj@gmail.com>
2017-08-28 17:01:37 -0700

    stripe events table, renamed stripe_id to stripe_event_id

--- db/migrate/20170801210139_create_influx_stripe_events.rb
+++ db/migrate/20170801210139_create_influx_stripe_events.rb
@@ -3,7 +3,7 @@ include Influx::MigrationHelpers
 class CreateInfluxStripeEvents < migration_base_class()
   def change
     create_table :influx_stripe_events do |t|
-      t.string   :stripe_id, index: true
+      t.string   :stripe_event_id, index: true
       t.timestamps
     end
   end
